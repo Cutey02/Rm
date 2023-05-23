2ab72018f06196288faa0b99e480bb83acaf45b7
~•~©√ʙᴀᴅ ʙᴏʏ™ <131782974+TeamRemo@users.noreply.github.com>
2023-05-23 21:21:04 +0530

    Update ping.py

--- RemoMusic/plugins/tools/ping.py
+++ RemoMusic/plugins/tools/ping.py
@@ -5,10 +5,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
 from strings import get_command
-from ShizukaXMusic import app
-from ShizukaXMusic.core.call import Shizuka
-from ShizukaXMusic.utils import bot_sys_stats
-from ShizukaXMusic.utils.decorators.language import language
+from RemoMusic import app
+from RemoMusic.core.call import Shizuka
+from RemoMusic.utils import bot_sys_stats
+from RemoMusic.utils.decorators.language import language
 
 ### Commands
 PING_COMMAND = get_command("PING_COMMAND")
@@ -24,7 +24,7 @@ async def ping_com(client, message: Message, _):
         caption=_["ping_1"],
     )
     start = datetime.now()
-    pytgping = await Shizuka.ping()
+    pytgping = await Remo.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
