9fc157dc43745960c1df8d606d67b46b47bd420c
~•~©√ʙᴀᴅ ʙᴏʏ™ <131782974+TeamRemo@users.noreply.github.com>
2023-05-23 19:30:59 +0530

    Update inline.py

--- RemoMusic/plugins/bot/inline.py
+++ RemoMusic/plugins/bot/inline.py
@@ -6,8 +6,8 @@ from pyrogram.types import (
 from youtubesearchpython.__future__ import VideosSearch
 
 from config import BANNED_USERS, MUSIC_BOT_NAME
-from ShizukaXMusic import app
-from ShizukaXMusic.utils.inlinequery import answer
+from RemoMusic import app
+from RemoMusic.utils.inlinequery import answer
 
 
 @app.on_inline_query(~BANNED_USERS)
