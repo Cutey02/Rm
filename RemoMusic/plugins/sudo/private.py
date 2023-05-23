053f3bdb2445336de0752d1308f37b3f1c55f96d
~•~©√ʙᴀᴅ ʙᴏʏ™ <131782974+TeamRemo@users.noreply.github.com>
2023-05-23 21:09:23 +0530

    Update private.py

--- RemoMusic/plugins/sudo/private.py
+++ RemoMusic/plugins/sudo/private.py
@@ -3,15 +3,15 @@ from pyrogram.types import Message
 
 import config
 from strings import get_command
-from ShizukaXMusic import app
-from ShizukaXMusic.misc import SUDOERS
-from ShizukaXMusic.utils.database import (
+from RemoMusic import app
+from RemoMusic.misc import SUDOERS
+from RemoMusic.utils.database import (
     add_private_chat,
     get_private_served_chats,
     is_served_private_chat,
     remove_private_chat,
 )
-from ShizukaXMusic.utils.decorators.language import language
+from RemoMusic.utils.decorators.language import language
 
 AUTHORIZE_COMMAND = get_command("AUTHORIZE_COMMAND")
 UNAUTHORIZE_COMMAND = get_command("UNAUTHORIZE_COMMAND")
