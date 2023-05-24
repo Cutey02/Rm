e1db783bb36250365db195eee6e8334c037678a6
Cutey02 <116083642+Cutey02@users.noreply.github.com>
2023-05-24 10:56:39 +0530

    Update __init__.py

--- RemoMusic/__init__.py
+++ RemoMusic/__init__.py
@@ -1,4 +1,4 @@
-from RemoMusic.core.bot import Remobot
+from RemoMusic.core.bot import RemoMusicbot
 from RemoMusic.core.dir import dirr
 from RemoMusic.core.git import git
 from RemoMusic.core.userbot import Userbot
