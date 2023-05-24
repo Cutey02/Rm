cc974f098e1ab73b9a37106e2ba7a69cba3a8bce
Cutey02 <116083642+Cutey02@users.noreply.github.com>
2023-05-24 09:37:00 +0530

    Update bot.py

--- RemoMusic/core/bot.py
+++ RemoMusic/core/bot.py
@@ -12,7 +12,7 @@ class RemoMusic(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            "Remo Music",
+            "RemoMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
