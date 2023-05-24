349f6f12a685b944c490377230c3fb7e7ab73977
Cutey02 <116083642+Cutey02@users.noreply.github.com>
2023-05-24 11:06:32 +0530

    Update __main__.py

--- RemoMusic/__main__.py
+++ RemoMusic/__main__.py
@@ -40,7 +40,7 @@ async def init():
     await userbot.start()
     await Remo.start()
     try:
-        await Shizuka.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await Remo.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("RemoMusic").error(
             "[ERROR] - \n\nTurn on group voice chat and don't put it off otherwise I'll stop working thanks."
